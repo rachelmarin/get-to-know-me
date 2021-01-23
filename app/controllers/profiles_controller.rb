5b8dca1116f57d729ef82d9dd92efa2ec9980ae9
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-22 20:17:04 -0500

    started working on profile controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,33 @@
+class ProfilesController < ApplicationController
+# before_action :find_profile, only: [:show, :edit, :update, :destroy]  
+
+def index
+    @profiles = Profile.all    
+end
+
+def new
+   @profile = Profile.new
+end
+
+def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+        redirect_to profiles_path
+    else
+        render :new
+    end
+end
+
+
+
+
+     private
+
+     def profile_params
+        params.require(:profile).permit(:name, :birthdate)       
+     end
+
+    # def find_profile
+    #     @profile = Profile.find_by_id(params[:id])
+    # end
+end
